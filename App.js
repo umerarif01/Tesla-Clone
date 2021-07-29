6b310afe08851f844f1d48754a5fa3287bdb1f14
unknown <umerarif01@gmail.com>
2021-07-30 02:54:14 +0500

    Tesla Clone

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View} from 'react-native';
+import Header from './Components/Header'
+import CarsList from './Components/CarsList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Header />
+       <CarsList />
+       
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +21,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  
 });
+{/* <CarItem name={"Model S"}
+       tagline={"Order Online For"}
+       taglineCTA={"Touchless Delivery"}
+       image={require('./assets/images/ModelS.jpeg')}
+       /> */}
